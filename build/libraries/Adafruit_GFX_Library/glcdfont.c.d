0f445b02f78b84c2625bac2986cf225ce7369e92
Astronat <c3142113@gmail.com>
2023-07-04 07:32:43 +1000

    incorrect build folder

--- build/libraries/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\nat\Documents\Arduino\fightstick\build\libraries\Adafruit_GFX_Library\glcdfont.c.o: \
- C:\Users\nat\Documents\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
--- build/libraries/Adafruit_NeoPixel/kendyte_k210.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\nat\Documents\Arduino\fightstick\build\libraries\Adafruit_NeoPixel\kendyte_k210.c.o: \
- C:\Users\nat\Documents\Arduino\libraries\Adafruit_NeoPixel\kendyte_k210.c
