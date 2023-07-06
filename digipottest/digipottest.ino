852856b2f714d9388491bcdcd57c45dc5499fe78
Astronat <c3142113@gmail.com>
2023-07-06 22:22:09 +1000

    added joystick disable pin to joystick selector + improved stick pos handling

--- digipottest/digipottest.ino
+++ digipottest/digipottest.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 
 #define LED_PIN 8
+#define DISABLE_PIN A0
 
 Adafruit_NeoPixel strip(2, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -16,19 +17,20 @@ Adafruit_NeoPixel strip(2, LED_PIN, NEO_GRB + NEO_KHZ800);
 #define DPAD_LEFT 1
 #define DPAD_DOWN 0
 
+static bool disable_output = false;
+
 MCP4131 pX(10);
 MCP4131 pY(9);
 
-
 enum stick_position {
-  TOP = 1, 
-  BOTTOM = 2, 
-  LEFT = 4, 
-  RIGHT = 8,
-  TOP_LEFT = TOP | LEFT,
-  TOP_RIGHT = TOP | RIGHT,
-  BOTTOM_LEFT = BOTTOM | LEFT,
-  BOTTOM_RIGHT = BOTTOM | RIGHT,
+  UP = 1 << 0, 
+  DOWN = 1 << 1, 
+  LEFT = 1 << 2, 
+  RIGHT = 1 << 3,
+  UP_LEFT = UP | LEFT,
+  UP_RIGHT = UP | RIGHT,
+  DOWN_LEFT = DOWN | LEFT,
+  DOWN_RIGHT = DOWN | RIGHT,
   CENTER = 0 
 };
 
@@ -43,21 +45,6 @@ static output_mode current_output_mode = DPAD;
 static stick_position stick_input = CENTER;
 static stick_position previous_stick_input = CENTER;
 
-static void read_stick() {
-  previous_stick_input = stick_input;
-  stick_position sp = CENTER;
-  if (digitalRead(INPUT_UP) == LOW) { sp = (stick_position)(sp | TOP); }
-  if (digitalRead(INPUT_DOWN) == LOW) { sp = (stick_position)(sp | BOTTOM); }
-  if (digitalRead(INPUT_LEFT) == LOW) { sp = (stick_position)(sp | LEFT); }
-  if (digitalRead(INPUT_RIGHT) == LOW) { sp = (stick_position)(sp | RIGHT); }
-
-  stick_input = sp;
-}
-
-void printBin(byte aByte) {
-  for (int8_t aBit = 3; aBit >= 0; aBit--)
-    Serial.write(bitRead(aByte, aBit) ? '1' : '0');
-}
 
 void setup() {
   Serial.begin(115200);
@@ -65,6 +52,8 @@ void setup() {
   strip.begin();
   //pinMode(5,OUTPUT);
   //pinMode(6,OUTPUT);
+
+  pinMode(DISABLE_PIN, INPUT_PULLUP);
 
   pinMode(INPUT_UP, INPUT_PULLUP);
   pinMode(INPUT_DOWN, INPUT_PULLUP);
@@ -84,47 +73,6 @@ void setup() {
   //pX.writeWiper(i);
 }
 
-
-static void set_dpad(stick_position sp) {
-  
-  digitalWrite(DPAD_UP,HIGH);
-  digitalWrite(DPAD_DOWN,HIGH);
-  digitalWrite(DPAD_LEFT,HIGH);
-  digitalWrite(DPAD_RIGHT,HIGH);
-
-  switch (sp) {
-    case LEFT: //Serial.println("left");         
-      digitalWrite(DPAD_LEFT,LOW);
-      break;
-    case TOP_LEFT: //Serial.println("top_left");
-      digitalWrite(DPAD_LEFT,LOW);
-      digitalWrite(DPAD_UP,LOW);
-      break;
-    case TOP: //Serial.println("top");
-      digitalWrite(DPAD_UP,LOW);
-      break;
-    case TOP_RIGHT: //Serial.println("top_right");
-      digitalWrite(DPAD_UP,LOW);
-      digitalWrite(DPAD_RIGHT,LOW);
-      break;
-    case RIGHT: //Serial.println("right");
-      digitalWrite(DPAD_RIGHT,LOW);
-      break;
-    case BOTTOM_RIGHT: //Serial.println("bottom_right");
-      digitalWrite(DPAD_DOWN,LOW);
-      digitalWrite(DPAD_RIGHT,LOW);
-      break;
-    case BOTTOM: //Serial.println("bottom");
-      digitalWrite(DPAD_DOWN,LOW);
-      break;
-    case BOTTOM_LEFT: //Serial.println("bottom_left");
-      digitalWrite(DPAD_DOWN,LOW);
-      digitalWrite(DPAD_LEFT,LOW);
-      break;
-    default: break;
-  }  
-}
-
 void loop() {
   strip.setPixelColor(0, 250,0,170);
   strip.setPixelColor(1, 250,0,170);
@@ -132,7 +80,13 @@ void loop() {
 
   read_stick();
  
-  if (stick_input != previous_stick_input) {
+  disable_output = digitalRead(DISABLE_PIN) == LOW;
+
+  if (disable_output) {
+    set_stick(CENTER);
+    set_dpad(CENTER);
+    
+  } else if (stick_input != previous_stick_input) {
     set_stick(stick_input);
     set_dpad(stick_input);
 
@@ -144,14 +98,14 @@ void loop() {
     print_stick_pos(previous_stick_input);
     
     Serial.print(" [state]");
-    printBin(stick_input);
+    printBinShort(stick_input);
     if (pressed != 0) {
       Serial.print(" [pressed]");
-      printBin(pressed);
+      printBinShort(pressed);
     }
     if (released != 0) {
       Serial.print(" [released]");
-      printBin(released);
+      printBinShort(released);
     }
     
     Serial.print("\n");    
@@ -163,46 +117,72 @@ void loop() {
 static void print_stick_pos(stick_position sp) {
   switch (sp) {
     case LEFT: Serial.print("left"); return;      
-    case TOP_LEFT: Serial.print("top_left"); return;
-    case TOP: Serial.print("top"); return;
-    case TOP_RIGHT: Serial.print("top_right"); return;
+    case UP_LEFT: Serial.print("top_left"); return;
+    case UP: Serial.print("top"); return;
+    case UP_RIGHT: Serial.print("top_right"); return;
     case RIGHT: Serial.print("right"); return;
-    case BOTTOM_RIGHT: Serial.print("bottom_right"); return;
-    case BOTTOM: Serial.print("bottom"); return;
-    case BOTTOM_LEFT: Serial.print("bottom_left"); return;
+    case DOWN_RIGHT: Serial.print("bottom_right"); return;
+    case DOWN: Serial.print("bottom"); return;
+    case DOWN_LEFT: Serial.print("bottom_left"); return;
     case CENTER: Serial.print("center"); return;
   }  
 }
 
-static void set_stick(stick_position sp) {  
+static void read_stick() {
+  previous_stick_input = stick_input;
+  stick_position sp = CENTER;
+  if (digitalRead(INPUT_UP) == LOW) { sp = (stick_position)(sp | UP); }
+  if (digitalRead(INPUT_DOWN) == LOW) { sp = (stick_position)(sp | DOWN); }
+  if (digitalRead(INPUT_LEFT) == LOW) { sp = (stick_position)(sp | LEFT); }
+  if (digitalRead(INPUT_RIGHT) == LOW) { sp = (stick_position)(sp | RIGHT); }
+  stick_input = sp;
+}
+
+void printBinShort(byte aByte) {
+  for (int8_t aBit = 3; aBit >= 0; aBit--)
+    Serial.write(bitRead(aByte, aBit) ? '1' : '0');
+}
+
+bool stick_has_pos(stick_position pos) {
+  return ((stick_input & pos) == pos);
+}
+
+static void set_dpad(stick_position sp) {  
+  digitalWrite(DPAD_UP,HIGH);
+  digitalWrite(DPAD_DOWN,HIGH);
+  digitalWrite(DPAD_LEFT,HIGH);
+  digitalWrite(DPAD_RIGHT,HIGH);
+
   switch (sp) {
-    case LEFT: //Serial.println("left");      
-      mj_left();
+    case LEFT:  
+      digitalWrite(DPAD_LEFT,LOW);
       break;
-    case TOP_LEFT: //Serial.println("top_left");
-      mj_top_left();
+    case UP_LEFT: 
+      digitalWrite(DPAD_LEFT,LOW);
+      digitalWrite(DPAD_UP,LOW);
       break;
-    case TOP: //Serial.println("top");
-      mj_top();
+    case UP:
+      digitalWrite(DPAD_UP,LOW);
       break;
-    case TOP_RIGHT: //Serial.println("top_right");
-      mj_top_right();
+    case UP_RIGHT:
+      digitalWrite(DPAD_UP,LOW);
+      digitalWrite(DPAD_RIGHT,LOW);
       break;
-    case RIGHT: //Serial.println("right");
-      mj_right();
+    case RIGHT:
+      digitalWrite(DPAD_RIGHT,LOW);
       break;
-    case BOTTOM_RIGHT: //Serial.println("bottom_right");
-      mj_bottom_right();
+    case DOWN_RIGHT:
+      digitalWrite(DPAD_DOWN,LOW);
+      digitalWrite(DPAD_RIGHT,LOW);
       break;
-    case BOTTOM: //Serial.println("bottom");
-      mj_bottom();
+    case DOWN: 
+      digitalWrite(DPAD_DOWN,LOW);
       break;
-    case BOTTOM_LEFT: //Serial.println("bottom_left");
-      mj_bottom_left();
+    case DOWN_LEFT: 
+      digitalWrite(DPAD_DOWN,LOW);
+      digitalWrite(DPAD_LEFT,LOW);
       break;
-    case CENTER: //Serial.println("center");
-      mj_center();
-      break;
+    default: break;
   }  
 }
 
@@ -211,20 +191,48 @@ void select_x() {
   digitalWrite(9,HIGH);
   digitalWrite(10,LOW);
 }
-
 void select_y() {
   digitalWrite(9,LOW);
   digitalWrite(10,HIGH);
 }
 
-
-//MOVE LEFT JOYSTICK
-void mj_left() { select_x(); pX.writeWiper(0); select_y(); pY.writeWiper(64); }
-void mj_top_left() { select_x(); pX.writeWiper(0); select_y(); pY.writeWiper(128); }
-void mj_top() { select_x(); pX.writeWiper(64); select_y(); pY.writeWiper(128); }
-void mj_top_right() { select_x(); pX.writeWiper(128); select_y(); pY.writeWiper(128); }
-void mj_right() { select_x(); pX.writeWiper(128); select_y(); pY.writeWiper(64);}
-void mj_bottom_right() { select_x(); pX.writeWiper(128); select_y(); pY.writeWiper(0); }
-void mj_bottom() { select_x(); pX.writeWiper(64); select_y(); pY.writeWiper(0); } 
-void mj_bottom_left() { select_x(); pX.writeWiper(0); select_y(); pY.writeWiper(0); }
-void mj_center() { select_x(); pX.writeWiper(64); select_y(); pY.writeWiper(64); }
+static void set_stick(stick_position sp) {  
+  switch (sp) {
+    case LEFT:  
+      select_x(); pX.writeWiper(0); 
+      select_y(); pY.writeWiper(64);
+      break;
+    case UP_LEFT:
+      select_x(); pX.writeWiper(0); 
+      select_y(); pY.writeWiper(128); 
+      break;
+    case UP:
+      select_x(); pX.writeWiper(64); 
+      select_y(); pY.writeWiper(128);
+      break;
+    case UP_RIGHT:
+      select_x(); pX.writeWiper(128); 
+      select_y(); pY.writeWiper(128); 
+      break;
+    case RIGHT:
+      select_x(); pX.writeWiper(128); 
+      select_y(); pY.writeWiper(64);
+      break;
+    case DOWN_RIGHT:
+      select_x(); pX.writeWiper(128); 
+      select_y(); pY.writeWiper(0); 
+      break;
+    case DOWN:
+       select_x(); pX.writeWiper(64); 
+       select_y(); pY.writeWiper(0);
+      break;
+    case DOWN_LEFT:
+      select_x(); pX.writeWiper(0); 
+      select_y(); pY.writeWiper(0);
+      break;
+    case CENTER:
+      select_x(); pX.writeWiper(64); 
+      select_y(); pY.writeWiper(64); 
+      break;
+  }  
+}
